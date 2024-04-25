47d98925d4eed28c781a5537b281d821c2d37268
Albie Spriddell <albion@dragonfruit.network>
2024-04-25 15:53:26 +0100

    update traceroute, remove null values from selectors

--- NetTools/Models/TracerouteResult.cs
+++ NetTools/Models/TracerouteResult.cs
@@ -2,10 +2,17 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text.Json.Serialization;
 
-namespace RoutingVisualiser.Models
-{
-    public record TracerouteResult(IPAddress DestinationIP, string DestinationName, IReadOnlyList<TracerouteHop> Hops, long Timestamp);
+namespace RoutingVisualiser.Models;
 
-    public record TracerouteHop(int Hop, IReadOnlyList<TracerouteProbe> Probes);
-    public record TracerouteProbe(string Annotation, int? ASN, IPAddress IP, string Name, [property: JsonPropertyName("rtt")] float RoundtripTimeMs);
-}
+public record TracerouteResult(
+    long Timestamp,
+    string DestinationName,
+    IPAddress DestinationIP,
+    IReadOnlyList<TracerouteHop> Hops);
+
+public record TracerouteHop(int Hop, IReadOnlyList<TracerouteProbe> Probes);
+
+public record TracerouteProbe(
+    IPAddress IP,
+    string Name,
+    [property: JsonPropertyName("rtt")] float RoundtripTimeMs);
