2b4ef558eddf25f2146c54b62cfca38eb8c00102
Albie Spriddell <albion@dragonfruit.network>
2024-04-25 19:19:01 +0100

    add route loss per-day chart

--- NetTools/Pages/Ping.razor.cs
+++ NetTools/Pages/Ping.razor.cs
@@ -33,16 +33,51 @@ public partial class Ping : ComponentBase
             }
         }
     };
-    
+
+    private readonly ApexChartOptions<PingResult> _hostPacketLossChartOptions = new()
+    {
+        Chart = new Chart
+        {
+            Stacked = true,
+            Height = "100%",
+            StackType = StackType.Percent100,
+        },
+        Title = new Title { Align = Align.Center },
+        Legend = new Legend { Position = LegendPosition.Bottom },
+        Theme = new Theme { Mode = Mode.Dark },
+        Colors = ["#4caf4f", "#ffeb3b", "#ffc107", "#f44336"],
+        PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = true } },
+        Xaxis = new XAxis
+        {
+            Labels = new XAxisLabels { Style = new AxisLabelStyle { FontSize = "12px" } },
+            Title = new AxisTitle
+            {
+                Text = "Percentage",
+                Style = new AxisTitleStyle
+                {
+                    FontSize = "14px"
+                }
+            }
+        },
+        Yaxis =
+        [
+            new YAxis
+            {
+                Title = new AxisTitle { Text = "Date", Style = new AxisTitleStyle { FontSize = "14px" } },
+                Labels = new YAxisLabels { Style = new AxisLabelStyle { FontSize = "12px" } }
+            }
+        ]
+    };
+
     private bool ShowUploadDialog { get; set; }
-    
+
     private ILookup<string, PingResult> PingResults { get; set; }
     private IGrouping<string, PingResult> SelectedHost { get; set; }
-    
+
     private async Task SelectedFileChanged(InputFileChangeEventArgs obj)
     {
         IReadOnlyCollection<PingResult> results;
-        
+
         switch (obj.File.ContentType)
         {
             case "application/json":
@@ -50,7 +85,7 @@ public partial class Ping : ComponentBase
                 using var stream = obj.File.OpenReadStream();
                 var result = await JsonSerializer.DeserializeAsync<PingResult>(stream, Program.JsonOptions);
                 results = new[] { result };
-                
+
                 break;
             }
 
@@ -64,14 +99,15 @@ public partial class Ping : ComponentBase
 
                 using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read, false);
                 var archiveResults = new List<PingResult>(archive.Entries.Count);
-                
+
                 foreach (var entry in archive.Entries.Where(x => Path.GetExtension(x.Name) == ".json"))
                 {
                     await using var entryStream = entry.Open();
 
                     try
                     {
-                        var result = await JsonSerializer.DeserializeAsync<PingResult>(entryStream, Program.JsonOptions);
+                        var result =
+                            await JsonSerializer.DeserializeAsync<PingResult>(entryStream, Program.JsonOptions);
                         archiveResults.Add(result);
                     }
                     catch
@@ -79,11 +115,11 @@ public partial class Ping : ComponentBase
                         // ignore deserialization errors
                     }
                 }
-                
+
                 results = archiveResults;
                 break;
             }
-            
+
             default:
                 return;
         }
