c09166e181ed16cee98f4476f21de74ecb27476c
Albie Spriddell <albion@dragonfruit.network>
2024-04-26 10:50:05 +0100

    add missing json types

--- NetTools/NetToolsSerializerContext.cs
+++ NetTools/NetToolsSerializerContext.cs
@@ -1,10 +1,12 @@
 using System.Text.Json.Serialization;
 using RoutingVisualiser.Geolocation;
+using RoutingVisualiser.Models;
 
 namespace RoutingVisualiser;
 
-[JsonSerializable(typeof(IpGeolocation))]
-[JsonSerializable(typeof(CachedIpGeolocation))]
+[JsonSerializable(typeof(IpGeolocation)), JsonSerializable(typeof(CachedIpGeolocation))]
+[JsonSerializable(typeof(TracerouteResult)), JsonSerializable(typeof(TracerouteProbe))]
+[JsonSerializable(typeof(PingResult)), JsonSerializable(typeof(PingResponse))]
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower, Converters = [typeof(JsonIPAddressConverter)])]
 internal partial class NetToolsSerializerContext : JsonSerializerContext
 {
