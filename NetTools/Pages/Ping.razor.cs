b90574cef721e9b677d048d4ff4d819a0b6dbe22
Albie Spriddell <albion@dragonfruit.network>
2024-04-26 10:23:48 +0100

    update component in ping

--- NetTools/Pages/Ping.razor.cs
+++ NetTools/Pages/Ping.razor.cs
@@ -69,62 +69,12 @@ public partial class Ping : ComponentBase
         ]
     };
 
-    private bool ShowUploadDialog { get; set; }
-
     private ILookup<string, PingResult> PingResults { get; set; }
     private IGrouping<string, PingResult> SelectedHost { get; set; }
 
-    private async Task SelectedFileChanged(InputFileChangeEventArgs obj)
+    private void SetProcessedItems(IReadOnlyCollection<PingResult> results)
     {
-        IReadOnlyCollection<PingResult> results;
-
-        switch (obj.File.ContentType)
-        {
-            case "application/json":
-            {
-                using var stream = obj.File.OpenReadStream();
-                var result = await JsonSerializer.DeserializeAsync<PingResult>(stream, Program.JsonOptions);
-                results = new[] { result };
-
-                break;
-            }
-
-            case "application/zip":
-            {
-                var memoryStream = new MemoryStream();
-                await using (var file = obj.File.OpenReadStream())
-                {
-                    await file.CopyToAsync(memoryStream).ConfigureAwait(false);
-                }
-
-                using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read, false);
-                var archiveResults = new List<PingResult>(archive.Entries.Count);
-
-                foreach (var entry in archive.Entries.Where(x => Path.GetExtension(x.Name) == ".json"))
-                {
-                    await using var entryStream = entry.Open();
-
-                    try
-                    {
-                        var result =
-                            await JsonSerializer.DeserializeAsync<PingResult>(entryStream, Program.JsonOptions);
-                        archiveResults.Add(result);
-                    }
-                    catch
-                    {
-                        // ignore deserialization errors
-                    }
-                }
-
-                results = archiveResults;
-                break;
-            }
-
-            default:
-                return;
-        }
-
         PingResults = results.OrderBy(x => x.Timestamp).ToLookup(x => x.Destination);
         SelectedHost = PingResults.Count == 1 ? PingResults.First() : null;
-    }
+     }
 }
