aab04d79fa3afee62664151af73801192eb973d0
Albie Spriddell <albion@dragonfruit.network>
2024-04-26 09:27:28 +0100

    add additional services

--- NetTools/Program.cs
+++ NetTools/Program.cs
@@ -1,7 +1,11 @@
 using System.IO;
+using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using DragonFruit.Data;
+using DragonFruit.Data.Serializers;
 using Havit.Blazor.Components.Web;
 using MaxMind.Db;
 using MaxMind.GeoIP2;
@@ -9,14 +13,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RoutingVisualiser.Geolocation;
+using Tavenem.Blazor.IndexedDB;
 
 namespace RoutingVisualiser;
 
 public class Program
 {
+    private const string IndexDbName = "nettools";
+    
     public static readonly JsonSerializerOptions JsonOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+        TypeInfoResolver = NetToolsSerializerContext.Default,
         Converters = { new JsonIPAddressConverter() }
     };
         
@@ -30,9 +39,26 @@ public class Program
 
         builder.Services.AddServerSideBlazor().AddHubOptions(config => config.MaximumReceiveMessageSize = 1048576);
         builder.Services.AddRazorPages();
-        builder.Services.AddHxServices();
-
         builder.Services.Configure<RazorPagesOptions>(c => c.RootDirectory = "/");
+
+        // blazor services
+        builder.Services.AddHxServices();
+        builder.Services.AddIndexedDb(new IndexedDb(IndexDbName, 1, "geocache"), JsonOptions);
+        builder.Services.AddBlazoredLocalStorageAsSingleton(c => c.JsonSerializerOptions = JsonOptions);
+        
+        builder.Services.AddSingleton<ApiClient>(_ =>
+        {
+            var client = new ApiClient<ApiJsonSerializer>
+            {
+                Handler = () => new HttpClientHandler() // wasm can't use the default socketshandler
+            };
+            
+            // enable proper IPAddress serialization
+            client.Serializers.Configure<ApiJsonSerializer>(s => s.SerializerOptions = JsonOptions);
+            return client;
+        });
+
+        builder.Services.AddSingleton<GeolocationService>();
 
         var geoIpFilePath = builder.Configuration["GeoIP:FilePath"];
         if (!string.IsNullOrEmpty(geoIpFilePath) && File.Exists(geoIpFilePath))
