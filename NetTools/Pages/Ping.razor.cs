b671d13eca4b719527a60042c91284977e78fcd8
Albie Spriddell <albion@dragonfruit.network>
2024-04-29 09:35:58 +0100

    add general host-invariant graphs

--- NetTools/Pages/Ping.razor.cs
+++ NetTools/Pages/Ping.razor.cs
@@ -1,19 +1,46 @@
 using System.Linq;
 using ApexCharts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
 using NetTools.Models;
 
 namespace NetTools.Pages;
 
 public partial class Ping : ComponentBase
 {
-    private ApexChart<PingResult> _packetLossChart, _hostPacketLossChart, _hostDualPingLossChart;
+    private ApexChart<PingResult> _responseTimeChart, _packetLossChart, _hostPacketLossChart, _hostDualPingLossChart;
+    
+    private readonly ApexChartOptions<PingResult> _responseTimeChartOptions = new()
+    {
+        Chart = new Chart
+        {
+            Stacked = true,
+            Height = "100%",
+            StackType = StackType.Percent100
+        },
+        Colors = ["#f44336", "#ffc107"],
+        Theme = new Theme { Mode = Mode.Dark },
+        Title = new Title { Align = Align.Center },
+        Legend = new Legend { Position = LegendPosition.Bottom },
+        PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = true } },
+        Xaxis = new XAxis { Type = XAxisType.Datetime, Title = new AxisTitle { Text = "Date" } },
+        Yaxis =
+        [
+            new YAxis
+            {
+                Title = new AxisTitle { Text = "RTT (ms)", Style = new AxisTitleStyle { FontSize = "14px" } },
+                Labels = new YAxisLabels { Style = new AxisLabelStyle { FontSize = "12px" } }
+            }
+        ]
+    };
     
     private readonly ApexChartOptions<PingResult> _packetLossChartOptions = new()
     {
         Theme = new Theme { Mode = Mode.Dark },
-        Chart = new Chart { Height = "100%", Stacked = true },
-        PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = false } }
+        Colors = ["#4caf4f", "#f44336"],
+        Legend = new Legend { Position = LegendPosition.Bottom },
+        PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = true } },
+        Chart = new Chart { Height = "100%", Stacked = true, StackType = StackType.Percent100 },
     };
 
     private readonly ApexChartOptions<PingResult> _hostPacketLossChartOptions = new()
@@ -93,12 +120,20 @@ public partial class Ping : ComponentBase
         ]
     };
 
+    [Inject]
+    private ILogger<Ping> Logger { get; set; }
+    
     private ILookup<string, PingResult> PingResults { get; set; }
     private IGrouping<string, PingResult> SelectedHost { get; set; }
 
     private void SetProcessedItems(ILookup<string, PingResult> results)
     {
         PingResults = results;
-        SelectedHost = PingResults.Count == 1 ? PingResults.First() : null;
-     }
+        SelectedHost = null;
+
+        if (PingResults.Count == 1)
+        {
+            SelectedHost = PingResults.Single();
+        }
+    }
 }
