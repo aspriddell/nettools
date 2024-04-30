9195e89d13e3762d4d020dd37a99be498eb8a3b0
Albie Spriddell <albion@dragonfruit.network>
2024-04-30 15:09:56 +0100

    make all ping charts consistent

--- NetTools/Pages/Ping.razor.cs
+++ NetTools/Pages/Ping.razor.cs
@@ -20,7 +20,6 @@ public partial class Ping : ComponentBase
         },
         Colors = ["#f44336", "#ffc107"],
         Theme = new Theme { Mode = Mode.Dark },
-        Title = new Title { Align = Align.Center },
         Legend = new Legend { Position = LegendPosition.Bottom },
         PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = true } },
         Xaxis = new XAxis { Type = XAxisType.Datetime, Title = new AxisTitle { Text = "Date" } },
@@ -36,8 +35,8 @@ public partial class Ping : ComponentBase
     
     private readonly ApexChartOptions<PingResult> _packetLossChartOptions = new()
     {
-        Theme = new Theme { Mode = Mode.Dark },
         Colors = ["#4caf4f", "#f44336"],
+        Theme = new Theme { Mode = Mode.Dark },
         Legend = new Legend { Position = LegendPosition.Bottom },
         PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = true } },
         Chart = new Chart { Height = "100%", Stacked = true, StackType = StackType.Percent100 },
@@ -45,16 +44,10 @@ public partial class Ping : ComponentBase
 
     private readonly ApexChartOptions<PingResult> _hostPacketLossChartOptions = new()
     {
-        Chart = new Chart
-        {
-            Stacked = true,
-            Height = "100%",
-            StackType = StackType.Percent100
-        },
-        Title = new Title { Align = Align.Center },
-        Legend = new Legend { Position = LegendPosition.Bottom },
         Theme = new Theme { Mode = Mode.Dark },
         Colors = ["#4caf4f", "#ffeb3b", "#ffc107", "#f44336"],
+        Legend = new Legend { Position = LegendPosition.Bottom },
+        Chart = new Chart { Stacked = true, Height = "100%", StackType = StackType.Percent100 },
         PlotOptions = new PlotOptions { Bar = new PlotOptionsBar { Horizontal = true } },
         Xaxis = new XAxis
         {
@@ -82,7 +75,6 @@ public partial class Ping : ComponentBase
     {
         Chart = new Chart { Height = "100%" },
         Theme = new Theme { Mode = Mode.Dark },
-        Title = new Title { Align = Align.Center },
         Tooltip = new Tooltip { X = new TooltipX { Format = "dd/MM/yyyy HH:mm" } },
         Xaxis = new XAxis
         {
