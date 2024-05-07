6666afbdaae1440a5b47759c0f0dd7f495efc6e7
Albie Spriddell <albion@dragonfruit.network>
2024-05-07 14:12:12 +0100

    set property names, allow null loss percent

--- NetTools/Models/PingResult.cs
+++ NetTools/Models/PingResult.cs
@@ -5,25 +5,25 @@ using System.Text.Json.Serialization;
 namespace NetTools.Models;
 
 public record PingResult(
-    IPAddress DestinationIP,
-    string Destination,
-    int DataBytes,
-    int PacketsTransmitted,
-    int PacketsReceived,
-    float PacketLossPercent,
-    float TimeMs,
-    float RoundTripTimeMin,
-    float RoundTripTimeMax,
-    float RoundTripTimeAvg,
-    float RoundTripTimeStddev,
-    long? Timestamp,
-    IReadOnlyList<PingResponse> Responses);
+    [property: JsonPropertyName("destination")] string Destination,
+    [property: JsonPropertyName("destination_ip")] IPAddress DestinationIP,
+    [property: JsonPropertyName("data_bytes")] int DataBytes,
+    [property: JsonPropertyName("packets_transmitted")] int PacketsTransmitted,
+    [property: JsonPropertyName("packets_received")] int PacketsReceived,
+    [property: JsonPropertyName("packet_loss_percent")] float? PacketLossPercent,
+    [property: JsonPropertyName("time_ms")] float TotalTimeMs,
+    [property: JsonPropertyName("round_trip_time_min")] float RoundTripTimeMin,
+    [property: JsonPropertyName("round_trip_time_max")] float RoundTripTimeMax,
+    [property: JsonPropertyName("round_trip_time_avg")] float RoundTripTimeAvg,
+    [property: JsonPropertyName("round_trip_time_stdev")] float RoundTripTimeStddev,
+    [property: JsonPropertyName("timestamp")] long? Timestamp,
+    [property: JsonPropertyName("responses")] IReadOnlyList<PingResponse> Responses);
 
 public record PingResponse(
-    string Type,
-    int Bytes,
-    IPAddress ResponseIP,
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("bytes")] int BytesSent,
+    [property: JsonPropertyName("response_ip")] IPAddress ResponseIP,
     [property: JsonPropertyName("icmp_seq")] int IMCPSeq,
-    int TTL,
-    float TimeMs,
-    bool Duplicate);
+    [property: JsonPropertyName("ttl")] int TTL,
+    [property: JsonPropertyName("time_ms")] float TimeMs,
+    [property: JsonPropertyName("duplicate")] bool Duplicate);
