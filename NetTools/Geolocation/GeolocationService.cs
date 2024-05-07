ed840c70619377a91226418d95cf31064870eb8d
Albie Spriddell <albion@dragonfruit.network>
2024-05-07 18:07:08 +0100

    cleanup using statements

--- NetTools/Geolocation/GeolocationService.cs
+++ NetTools/Geolocation/GeolocationService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using DragonFruit.Data;
